0293e903eb8aa93c05e0717e0b858dab1989c8e8
MarJC5 <jmartin@student.42lausanne.ch>
2021-10-25 17:24:41 +0200

    Update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 11:25:47 by jmartin           #+#    #+#             */
-/*   Updated: 2021/10/25 16:54:16 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/10/25 17:24:21 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,20 @@ int	main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		ft_putendl_fd("Pleaser enter the file descriptor path after the program name.", 2);
-		return (0);
+		return (1);
 	}
 	else
 		path = argv[argc - 1];
 	if ((fd = open(path, O_RDONLY)) == -1)
 	{
-		ft_putendl_fd("Cannot open the file", 2);
-		return (0);
+		ft_putendl_fd("Cannot open the file.", 2);
+		return (1);
 	}
 	get_next_line(fd);
+	if (close(fd) == -1)
+	{
+		ft_putendl_fd("Error, cannot close the file.", 2);
+		return (1);
+	}
 	return (0);
 }
