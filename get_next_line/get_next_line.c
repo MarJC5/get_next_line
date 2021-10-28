c78f86e87c1f759e30c31e4401265f9a4ef50977
Martin Jean-Christio <admin@marjc.ch>
2021-10-29 00:11:34 +0200

    Update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,29 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 11:25:47 by jmartin           #+#    #+#             */
-/*   Updated: 2021/10/29 00:04:05 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/10/29 00:10:26 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static int	ft_strchr_pos(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(str) + 1)
+	{
+		if (str[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
 static char	*ft_read_check(int fd, char **save, char *buf, char *result)
 {
-	char 	*nl;
+	char	*nl;
 	int		file;
 
 	nl = NULL;
@@ -42,7 +56,6 @@ static char	*ft_line(int fd, char **save, char *buf)
 	int		c;
 
 	c = 0;
-
 	result = ft_read_check(fd, save, buf, result);
 	while (result[c] != '\n')
 		c++;
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 11:26:08 by jmartin           #+#    #+#             */
-/*   Updated: 2021/10/29 00:03:48 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/10/29 00:10:35 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,6 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(const char *str, unsigned int start, size_t len);
 char	*ft_strchr(const char *str, int c);
-
-int     ft_strchr_pos(const char *str, int c);
-
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
