a709d23c21276e1072a393549064f4b1722b2001
Martin Jean-Christio <admin@marjc.ch>
2021-11-15 01:48:11 +0100

    Update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 11:25:47 by jmartin           #+#    #+#             */
-/*   Updated: 2021/11/15 00:23:11 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/11/15 01:46:59 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,27 @@ static void	free_ptr(char **ptr)
 	}
 }
 
-static char	*ft_return_line(char **save)
+static char	*ft_return_line(char **save, char *res)
 {
 	int		i;
-	char	*res;
 	char	*tmp;
 
 	i = 0;
 	tmp = NULL;
-	res = NULL;
 	while ((*save)[i] != '\n' && (*save)[i])
 		i++;
 	if ((*save)[i++] == '\n')
 		res = ft_substr(*save, 0, i);
-	else if (ft_strchr(*save, '\n') == NULL)
+	else if (ft_strchr(*save, '\0'))
 	{
-		if (ft_strlen(*save) == 0 && ft_strchr(*save, '\0'))
+		if (**save == '\0')
+		{
+			free_ptr(save);
 			return (NULL);
-		res = ft_strdup(*save);
+		}
+		res = *save;
+		*save = NULL;
+		return (res);
 	}
 	tmp = ft_strdup(*save + i);
 	free(*save);
@@ -50,25 +53,23 @@ static char	*ft_read_file(int fd, char **save, char *buf)
 {
 	int		file;
 	char	*tmp;
+	char	*res;
 
+	res = NULL;
 	file = read(fd, buf, BUFFER_SIZE);
 	while (file)
 	{
 		buf[file] = '\0';
-		if (*save)
-		{
-			tmp = *save;
-			*save = ft_strjoin(*save, buf);
-			free(tmp);
-		}
-		else
-			*save = ft_substr(buf, 0, file);
+		tmp = ft_strjoin(*save, buf);
+		free(*save);
+		*save = ft_strdup(tmp);
+		free_ptr(&tmp);
 		if (ft_strchr(buf, '\n'))
 			break ;
 		file = read(fd, buf, BUFFER_SIZE);
 	}
 	free_ptr(&buf);
-	return (ft_return_line(save));
+	return (ft_return_line(save, res));
 }
 
 char	*get_next_line(int fd)
